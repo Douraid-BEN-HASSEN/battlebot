f8e70ebe5617cfe0956a596d2affbbff14705300
gauthier bizzarri <gauthier-tristant.bizzarri@imerir.com>
2023-02-06 16:21:18 +0100

    send mqtt when order is created

--- server/api_app/serializers.py
+++ server/api_app/serializers.py
@@ -1,8 +1,9 @@
 # countries/serializers.py
 from rest_framework import serializers
 from .models import Order
-
+import paho.mqtt.client as mqtt
 from datetime import datetime
+
 class OrderSerializer(serializers.ModelSerializer):
     received_time = serializers.DateTimeField(default=datetime.now)
 
@@ -13,4 +14,10 @@ class OrderSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         received_time = datetime.now().isoformat()
         validated_data["received_time"] = datetime.fromisoformat(received_time)
-        return Order.objects.create(**validated_data)
+        order =  Order.objects.create(**validated_data)
+        client = mqtt.Client()
+        client.connect("broker.emqx.io", 1883)
+        client.publish(order.topic, order.message)
+        client.disconnect()
+        return order
+
