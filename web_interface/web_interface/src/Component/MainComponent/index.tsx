a21d11c22a64753cd39b8332da21b08cdb714a1f
Lucas AUXACHS <lucas.auxachs@imerir.com>
2023-02-06 17:08:20 +0100

    Update Component History

--- web_interface/web_interface/src/Component/MainComponent/index.tsx
+++ web_interface/web_interface/src/Component/MainComponent/index.tsx
@@ -7,52 +7,38 @@ import Historique from "../Historique";
 
 interface mainComponentProps {}
 
+const keyToAction = {
+  z: "Ordre : Avance",
+  q: "Ordre : Tourne à gauche",
+  s: "Ordre : Recule ",
+  d: "Ordre : Tourne à droite",
+  o: "Ordre : Action 1",
+  p: "Ordre : Action 2",
+} as any;
+
 const MainComponent: React.FC<mainComponentProps> = React.memo(({}) => {
-  const [datasHistory, setDatasHistory] = useState<Array<string>>([
-    "bonjour",
-    "je",
-    "m'appelle",
-    "douraid",
-    "bonjour",
-    "je",
-    "m'appelle",
-    "douraid",
-    "bonjour",
-    "je",
-    "m'appelle",
-    "douraid",
-    "bonjour",
-    "je",
-    "m'appelle",
-    "douraid",
-    "bonjour",
-    "je",
-    "m'appelle",
-    "douraid",
-    "bonjour",
-    "je",
-    "m'appelle",
-    "douraid",
-    "bonjour",
-    "je",
-    "m'appelle",
-    "douraid",
-  ]);
+  const [datasHistory, setDatasHistory] = useState<Array<string>>([]);
 
   const onClearHistory = () => {
     setDatasHistory([]);
   };
 
+  const handleAddHistory = (value: string) => {
+    console.log("value : ", value);
+    let tmp = datasHistory as any;
+    tmp.push(keyToAction[value]);
+    setDatasHistory(tmp)
+  };
+
   return (
     <>
       <Header />
-
       <Grid container spacing={1} style={{ width: "95%", marginLeft: "2.5px" }}>
         <Grid item xs={6}>
           <Historique datasHistory={datasHistory} handleClearHistory={onClearHistory} />
         </Grid>
         <Grid item xs={6}>
-          <Controller />
+          <Controller handleAddHistory={handleAddHistory} />
         </Grid>
       </Grid>
 
