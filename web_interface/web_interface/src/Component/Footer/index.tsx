12ff9cbf365943cb800f3f6e5713b31fe5d54679
Lucas AUXACHS <lucas.auxachs@imerir.com>
2023-02-06 15:29:01 +0100

    Update Footer

--- web_interface/web_interface/src/Component/Footer/index.tsx
+++ web_interface/web_interface/src/Component/Footer/index.tsx
@@ -5,6 +5,7 @@ import React, { useState } from "react";
 interface footerProrps {}
 
 const Footer: React.FC<footerProrps> = React.memo(({}) => {
+  const [isUpdate, setIsUpdate] = useState<boolean>(false);
   const [addresse, setAddresse] = useState<string>("");
   const [port, setPort] = useState<number>();
 
@@ -12,13 +13,14 @@ const Footer: React.FC<footerProrps> = React.memo(({}) => {
     <Box sx={{ flewGrow: 1 }} style={{ position: "fixed", left: "0", bottom: "0", width: "100%" }}>
       <AppBar
         position="sticky"
-        style={{ backgroundColor: "lightblue", borderTop: "3px solid #3f51b5" , padding:'10px'}}
+        style={{ backgroundColor: "lightblue", borderTop: "3px solid #3f51b5", padding: "10px" }}
       >
         <Toolbar>
           <Grid container spacing={1} justifyContent="center">
             <Grid item xs={6} style={{ fontWeight: "bold" }}>
               <Box display="flex" justifyContent="center" alignItems="center">
                 <TextField
+                  disabled={!isUpdate}
                   style={{ width: "100%" }}
                   label="Adresse IP"
                   onChange={(event: any) => {
@@ -30,7 +32,8 @@ const Footer: React.FC<footerProrps> = React.memo(({}) => {
             <Grid item xs={3} style={{ fontWeight: "bold" }}>
               <Box display="flex" justifyContent="center" alignItems="center">
                 <TextField
-                  style={{ width: "100%"  }}
+                  disabled={!isUpdate}
+                  style={{ width: "100%" }}
                   label="Port"
                   type={"number"}
                   onChange={(event: any) => {
@@ -46,12 +49,16 @@ const Footer: React.FC<footerProrps> = React.memo(({}) => {
                     color="inherit"
                     variant="contained"
                     style={{ width: "95%", color: "black", fontWeight: "bold" }}
+                    onClick={() => {
+                      if (isUpdate === true) setIsUpdate(false);
+                      else setIsUpdate(true);
+                    }}
                   >
-                    Modifier
+                    {isUpdate ? "Confirmer" : "Modifier"}
                   </Button>
                 </Box>
               </Grid>
-              <Grid item xs={12} style={{ fontWeight: "bold" , marginTop:'5px'}}>
+              <Grid item xs={12} style={{ fontWeight: "bold", marginTop: "5px" }}>
                 <Box display="flex" justifyContent="center" alignItems="center">
                   <Button
                     color="inherit"
