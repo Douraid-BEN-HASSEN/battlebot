00711b9739275b76020f8fa285b23ac8b3aad8a3
Lucas AUXACHS <lucas.auxachs@imerir.com>
2023-02-09 09:11:01 +0100

    Update UI

---
+++ web_interface/web_interface/src/Component/CurrentAction/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { TYPE_STYLE } from "../../Constantes/Types";
+import {Grid} from '@mui/material'
+
+interface currentActionProps {
+  action: string;
+  style: TYPE_STYLE;
+}
+
+const CurrentAction: React.FC<currentActionProps> = React.memo(({ action, style }) => {
+  console.log("last action : ", action);
+
+  return <Grid
+  container
+  spacing={0}
+  style={{
+    marginTop:'30px',
+    border: style.borderWidth + " solid " + style.borderColor,
+    borderRadius: style.borderRadius,
+    padding: "20px",
+    backgroundColor: style.backgroundColor,
+  }}
+>
+    <Grid item xs={12} style={{fontWeight:'bold' , fontSize:'20px'}}>
+        Ordre actuel : 
+    </Grid>
+    <Grid item xs={12} style={{marginTop:'30px', fontWeight:'bold' , fontSize:'20px'}}>
+        {action}
+    </Grid>
+</Grid>;
+});
+
+export default CurrentAction;
---
+++ web_interface/web_interface/src/Component/SwitchMode/index.tsx
@@ -0,0 +1,50 @@
+import { Button, Grid } from "@mui/material";
+import React, { useState } from "react";
+import { TYPE_STYLE } from "../../Constantes/Types";
+
+interface switchModeProps {
+  style: TYPE_STYLE;
+  handleChangeMode: (mode: "avance" | "basique") => void;
+}
+
+const SwitchMode: React.FC<switchModeProps> = React.memo(({ style, handleChangeMode }) => {
+  const [mode, setMode] = useState<"basique" | "avance">("avance");
+
+  return (
+    <Grid
+      container
+      spacing={0}
+      style={{
+        border: style.borderWidth + " solid " + style.borderColor,
+        borderRadius: style.borderRadius,
+        padding: "20px",
+        marginBottom: "30px",
+        backgroundColor: style.backgroundColor,
+      }}
+    >
+      <Grid item xs={12}>
+        <Button
+          onClick={() => {
+            if (mode === "avance") {
+              setMode("basique");
+              handleChangeMode("basique");
+            } else {
+              setMode("avance");
+              handleChangeMode("avance");
+            }
+          }}
+          variant="contained"
+          color="inherit"
+          style={{
+            width: "100%",
+            border: style.borderWidthButton + " solid " + style.borderColorButton,
+          }}
+        >
+          Passe en mode {mode === "avance" ? "Basique" : "Avancé"}
+        </Button>
+      </Grid>
+    </Grid>
+  );
+});
+
+export default SwitchMode;
